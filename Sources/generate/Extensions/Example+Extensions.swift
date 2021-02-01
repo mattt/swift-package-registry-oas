cce5935081e28a1eb7e02d4e10118ba84f82d50c
Mattt <mattt@me.com>
2021-02-01 10:22:07 -0800

    Improve ReDoc output by encoding examples as JSON #3 (#4)
    * show an idea for encoding examples as JSON for the benefit of ReDoc.
    
    * Refactor original implementation
    
    Co-authored-by: Mathew Polzin <matt.polzin@gmail.com>

--- Sources/generate/Extensions/Example+Extensions.swift
+++ Sources/generate/Extensions/Example+Extensions.swift
@@ -2,6 +2,10 @@ import OpenAPIKit
 
 extension OpenAPI.Example: ExpressibleByStringLiteral {
     public init(stringLiteral value: String) {
-        self.init(value: .init(AnyCodable(value)))
+        do {
+            try self.init(value: .init(.decoding(json: value)))
+        } catch {
+            self.init(value: .init(.init(value)))
+        }
     }
 }
